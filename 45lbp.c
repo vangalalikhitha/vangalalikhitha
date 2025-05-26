c726abaa0f397214c399e39db624ad1fa4119b4c
vangalalikhitha <vangalalikitha54@gmail.com>
2025-05-26 10:43:40 +0530

    Add files via upload

---
+++ 45lbp.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the values:");
+	scanf("%d,%d",&a,&b);
+	{
+		c=(a-b)*(a-b);
+	}
+    printf("%d",c);
+	return 0;
+	
+}
